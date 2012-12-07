5b7e8dd46d113628bc3727805ff329bf0e42bea5
Frank Wienberg <frank.wienberg@coremedia.com>
2012-12-07 10:59:08 +0100

    fixes IDEA-23 Jangaroo 0.9 plugin does not work with IDEA 12 http://jangaroo.myjetbrains.com/youtrack/issue/IDEA-23
    Now works with IDEA 10.5, 11, and 12.
    Fixed by invoking the two methods whose signature changed
    by reflection.
    On the fly, bump Java language level to 1.6.

--- idea-plugin/src/main/java/net/jangaroo/ide/idea/JangarooSdkUtils.java
+++ idea-plugin/src/main/java/net/jangaroo/ide/idea/JangarooSdkUtils.java
@@ -12,6 +12,9 @@ import com.intellij.peer.PeerFactory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA. User: fwienber Date: 27.11.11 Time: 23:49 To change this template use File | Settings |
  * File Templates.
@@ -23,9 +26,8 @@ public class JangarooSdkUtils {
     if (sdkHomePath == null || LocalFileSystem.getInstance().refreshAndFindFileByPath(sdkHomePath) == null) {
       return null;
     }
-    ProjectJdkTable projectJdkTable = ProjectJdkTable.getInstance();
     String sdkHomePathNormalized = sdkHomePath.replace('\\', '/');
-    for (Sdk sdk : projectJdkTable.getSdksOfType(sdkType)) {
+    for (Sdk sdk : getSdksOfType(sdkType)) {
       String homePath = sdk.getHomePath();
       if (homePath != null && homePath.replace('\\', '/').equals(sdkHomePathNormalized)) {
         if (sdkType instanceof JangarooSdkType) {
@@ -62,6 +64,44 @@ public class JangarooSdkUtils {
         return sdk;
       }
     });
+  }
+
+  /**
+   * Invoke Sdk#getSdkType() by reflection because of binary incompatibility between IDEA 11 and 12.
+   */
+  public static SdkType getSdkType(Sdk sdk) {
+    return (SdkType)invoke(sdk, "getSdkType", new Class[0], new Object[0]);
+  }
+
+  /**
+   * Invoke ProjectJskTable#getSdksOfType() by reflection because of binary incompatibility between IDEA 11 and 12.
+   */
+  @SuppressWarnings("unchecked")
+  private static List<Sdk> getSdksOfType(SdkType sdkType) {
+    Class<?> sdkTypeClass;
+    try {
+      sdkTypeClass = Class.forName("com.intellij.openapi.projectRoots.SdkTypeId");
+    } catch (ClassNotFoundException e) {
+      sdkTypeClass = SdkType.class; 
+    }
+    return (List<Sdk>)invoke(ProjectJdkTable.getInstance(), "getSdksOfType", new Class[]{ sdkTypeClass }, new Object[]{ sdkType });
+  }
+
+  private static Object invoke(Object object, String methodName, Class[] argTypes, Object[] args) {
+    Object result;
+    try {
+      result = object.getClass().getMethod(methodName, argTypes).invoke(object, args);
+    } catch (IllegalAccessException e) {
+      throw new IllegalStateException(e);
+    } catch (InvocationTargetException e) {
+      Throwable targetException = e.getTargetException();
+      throw targetException instanceof RuntimeException
+        ? (RuntimeException)targetException
+        : new RuntimeException(e);
+    } catch (NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
+    return result;
   }
 
 }
