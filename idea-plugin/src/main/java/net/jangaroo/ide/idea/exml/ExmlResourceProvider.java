7bbeaff0b59330991f49ac5948c9220e067dfa3d
Frank Wienberg <frank.wienberg@coremedia.com>
2013-04-05 10:34:46 +0200

    implements IDEA-25 Tell IDEA about custom Jangaroo ActionScript annotations http://jangaroo.myjetbrains.com/youtrack/issue/IDEA-25
    Provide a copy of IDEA's "KnownMetaData.dtd", extended
    by Jangaroo's custom ActionScript annotations like [Native],
    [Accessor], [ExtConfig] and [RestResource].
    This even contains some documentation of the annotations.

--- idea-plugin/src/main/java/net/jangaroo/ide/idea/exml/ExmlResourceProvider.java
+++ idea-plugin/src/main/java/net/jangaroo/ide/idea/exml/ExmlResourceProvider.java
@@ -12,6 +12,7 @@ public class ExmlResourceProvider implements StandardResourceProvider {
   public void registerResources(ResourceRegistrar resourceRegistrar) {
     resourceRegistrar.addStdResource(Exmlc.EXML_NAMESPACE_URI, Exmlc.EXML_SCHEMA_LOCATION, getClass());
     resourceRegistrar.addStdResource(Exmlc.EXML_UNTYPED_NAMESPACE_URI, Exmlc.EXML_UNTYPED_SCHEMA_LOCATION, getClass());
+    resourceRegistrar.addStdResource("urn:Flex:Meta", "/META-INF/KnownMetaData.dtd", getClass());
   }
 
 }
