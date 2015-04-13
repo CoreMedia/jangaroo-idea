c40ddc3578226aca7077dd7fd807118d7d6e67e1
Frank Wienberg <frank.wienberg@coremedia.com>
2015-04-13 18:18:51 +0200

    Enable source-mapped ActionScript breakpoints for JavaScript debugging.
    #thanks Alexander Doroshko (JetBrains)!

--- idea-plugin/src/main/java/net/jangaroo/ide/idea/debugger/JangarooJavaScriptDebugAware.java
+++ idea-plugin/src/main/java/net/jangaroo/ide/idea/debugger/JangarooJavaScriptDebugAware.java
@@ -1,7 +1,9 @@
 package net.jangaroo.ide.idea.debugger;
 
 import com.intellij.lang.javascript.ActionScriptFileType;
+import com.intellij.lang.javascript.flex.debug.FlexBreakpointType;
 import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import com.jetbrains.javascript.debugger.JavaScriptDebugAware;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,5 +15,11 @@ public class JangarooJavaScriptDebugAware extends JavaScriptDebugAware {
   @Override
   public LanguageFileType getFileType() {
     return ActionScriptFileType.INSTANCE;
+  }
+
+  @Nullable
+  @Override
+  public Class<? extends XLineBreakpointType<?>> getBreakpointTypeClass() {
+    return FlexBreakpointType.class;
   }
 }
