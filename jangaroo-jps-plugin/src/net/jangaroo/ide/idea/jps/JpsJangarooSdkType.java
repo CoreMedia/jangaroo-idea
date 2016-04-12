a397d55bf625b6fe6a007bbc3e4eb04aa1bb8458
Frank Wienberg <frank.wienberg@coremedia.com>
2016-04-12 09:59:34 +0200

    Properties compiler still uses commons-lang 2.x. :-(
    It seems that while testing, the corresponding JAR was there by coincidence,
    but you cannot rely on this. Thus, we now configure both commons-lang 2.6
    and commons-lang3.
    On the fly, introduced a new internal Jangaroo IDEA Plugin version, so that it
    is easier to see which preliminary version is installed.

--- jangaroo-jps-plugin/src/net/jangaroo/ide/idea/jps/JpsJangarooSdkType.java
+++ jangaroo-jps-plugin/src/net/jangaroo/ide/idea/jps/JpsJangarooSdkType.java
@@ -32,6 +32,7 @@ public class JpsJangarooSdkType extends JpsSdkType<JpsDummyElement> {
     "commons-configuration:commons-configuration:1.10",
     "commons-io:commons-io:2.4",
     "commons-collections:commons-collections:3.2.1",
+    "commons-lang:commons-lang:2.6",
     "org.apache.commons:commons-lang3:3.2.1",
     "commons-logging:commons-logging:1.1.1",
     "org.freemarker:freemarker:2.3.15",
