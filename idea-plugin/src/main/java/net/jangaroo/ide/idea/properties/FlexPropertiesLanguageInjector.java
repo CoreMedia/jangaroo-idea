5c5f4cd4f1150e4e10119c26a402ef5779e19b35
Frank Wienberg <frank.wienberg@coremedia.com>
2016-04-25 16:10:47 +0200

    IntelliSense support for annotations in properties files.
    Flex allows (some) annotations as property values in properties files.
    This naive implementation just injects AS3 language when a property
    value matches a regular expression (first-upper-case identifier followed
    by opening bracket, ending with closing bracket).
    Thus, typing "key=E()" allows completion to "Embed()" and completion /
    reference check of the source file to embed.
    Currently, any ActionScript annotation works, so we can still use
    Resource(key='...', bundle='...') cross references.
    
    On the fly, increased plugin version to 4.0.3.

---
+++ idea-plugin/src/main/java/net/jangaroo/ide/idea/properties/FlexPropertiesLanguageInjector.java
@@ -0,0 +1,39 @@
+package net.jangaroo.ide.idea.properties;
+
+import com.intellij.lang.javascript.JavaScriptSupportLoader;
+import com.intellij.lang.properties.IProperty;
+import com.intellij.lang.properties.psi.PropertiesFile;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.InjectedLanguagePlaces;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
+
+/**
+ * Inject annotations like Embed('/resources/image.png') into Flex properties files.
+ */
+public class FlexPropertiesLanguageInjector implements LanguageInjector {
+
+  private static final Pattern ANNOTATION_PATTERN = Pattern.compile(
+    "^\\s*[A-Z][a-zA-Z0-9]+\\s*\\([^)]*\\)\\s*$"
+  );
+
+  @Override
+  public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost psiLanguageInjectionHost,
+                                   @NotNull InjectedLanguagePlaces injectedLanguagePlaces) {
+    PsiFile psiFile = psiLanguageInjectionHost.getContainingFile();
+    if (psiFile instanceof PropertiesFile &&
+      psiLanguageInjectionHost instanceof IProperty) {
+      String value = ((IProperty)psiLanguageInjectionHost).getValue();
+      if (value != null && ANNOTATION_PATTERN.matcher(value).matches()) {
+        PsiElement valueElement = psiLanguageInjectionHost.getLastChild();
+        injectedLanguagePlaces.addPlace(JavaScriptSupportLoader.ECMA_SCRIPT_L4,
+          TextRange.from(valueElement.getStartOffsetInParent(), valueElement.getTextLength()), "[", "]");
+      }
+    }
+  }
+}
