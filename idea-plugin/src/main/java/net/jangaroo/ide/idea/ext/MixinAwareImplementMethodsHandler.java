66e35ea1053b4c2e25f532c90ae7812fc5167b35
fwienber <frank.wienberg@coremedia.com>
2018-06-05 18:45:37 +0200

    Adapt to IDEA 2018.1: API JavaScriptImplementMethodsHandler Changed
    It seems impossible to write our code such that both IDEA 2017.3 and
    2018.1 are happy. :-(
    Instead of subclassing JavaScriptImplementMethodsHandler, we now use
    JavaScriptImplementMethodsHandlerForFlex used by original FlexSupport
    plugin.

--- idea-plugin/src/main/java/net/jangaroo/ide/idea/ext/MixinAwareImplementMethodsHandler.java
+++ idea-plugin/src/main/java/net/jangaroo/ide/idea/ext/MixinAwareImplementMethodsHandler.java
@@ -1,18 +1,19 @@
 package net.jangaroo.ide.idea.ext;
 
-import com.intellij.lang.javascript.generation.JavaScriptImplementMethodsHandler;
+import com.intellij.lang.javascript.generation.JavaScriptImplementMethodsHandlerForFlex;
 import com.intellij.lang.javascript.psi.ecmal4.JSClass;
-import com.intellij.lang.javascript.validation.fixes.BaseCreateMethodsFix;
+import com.intellij.lang.javascript.validation.fixes.BaseCreateMembersFix;
+import com.intellij.psi.PsiElement;
 
 /**
  * Handler for generating method implementations so that methods of an implemented Ext Mixin interface are implemented
  * as native methods.
  */
-public class MixinAwareImplementMethodsHandler extends JavaScriptImplementMethodsHandler {
+public class MixinAwareImplementMethodsHandler extends JavaScriptImplementMethodsHandlerForFlex {
 
   @Override
-  protected BaseCreateMethodsFix createFix(JSClass clazz) {
-    return new MixinAwareImplementMethodsFix(clazz);
+  protected BaseCreateMembersFix createFix(PsiElement clazz) {
+    return new MixinAwareImplementMethodsFix((JSClass)clazz);
   }
 
 }
